2464e865e12509b759224fb436482aa1b240a4a6
Caroline Tavaress <ca.tavares7960@gmail.com>
2024-04-21 22:06:11 -0300

    atualizando git

--- aula25-03/index.js
+++ aula25-03/index.js
@@ -3,9 +3,10 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            heroi: {vida: 100},
-            vilao: {vida: 100},
+            heroi: {vida: 100, defendendo: false},
+            vilao: {vida: 100, defendendo: false},
             quantidade_pocao: 2,
+            quantidade_pocao_vilao: 2,
             turno: true,
             botao_clicado: false,
             jogo_ativo: true,
@@ -25,6 +26,7 @@ createApp({
                 this.ativar_gif('./imagens/ataque_sailormoon.gif');
             } else {
                 this.heroi.vida -= 20;
+                this.ativar_gif_vilao('./imagens/koan_ataque.gif');
             }
             this.botao_desativado = true;
             this.fim_de_jogo();
@@ -32,6 +34,8 @@ createApp({
         defender(isHeroi) {
             if(isHeroi){
                 this.ativar_gif('./imagens/defesa_sailormoon.gif');
+            } else {
+                this.ativar_gif_vilao('./imagens/koan_defesa.gif');
             }
             this.botao_desativado = true; 
         },
@@ -41,10 +45,15 @@ createApp({
                     this.heroi.vida += 20;
                     this.quantidade_pocao -= 1;
                     this.ativar_gif('./imagens/cura_sailormoon.gif');
+                } else {
+                    alert("Você usou todas as poções!")
                 }
             } else {
-                alert("Você usou todas as poções!")
-                this.botao_desativado = false;
+                if(this.vilao.vida <=50 && this.quantidade_pocao_vilao > 0){
+                    this.vilao.vida += 10;
+                    this.quantidade_pocao_vilao -= 1;
+                    this.ativar_gif_vilao('./imagens/koan_cura.gif');
+                }
             }
             this.botao_desativado = true;
         },
@@ -52,21 +61,54 @@ createApp({
             if(isHeroi){
                 this.heroi.vida = 100
                 this.vilao.vida = 100
+                this.quantidade_pocao = 2
+                this.quantidade_pocao_vilao = 2;
                 this.turno = true;
                 this.ativar_gif('./imagens/correndo_sailor.gif');
                 alert("A heroína correu!")
+                this.botao_desativado = true; 
+                clearInterval(this.timer); 
+                setTimeout(() => {
+                    this.segundos = 5;
+                    this.botao_desativado = false; 
+                    this.iniciar_timer(); 
+                }, 2000);
+            } else {
+                this.vilao.vida = 100;
+                this.heroi.vida = 100
+                this.quantidade_pocao = 2
+                this.quantidade_pocao_vilao = 2;
+                this.turno = false;
+                this.ativar_gif_vilao('./imagens/koan_correndo.gif');
+                alert("A vilã correu")
+                this.botao_desativado = true;
+                clearInterval(this.timer);
+                setTimeout(() => {
+                    this.segundos = 5;
+                    this.botao_desativado = false;
+                    this.iniciar_timer();
+                }, 3000);
             }
-            this.botao_desativado = false;
         },
         ativar_gif(imagem){
             this.imagem_heroi = imagem;
             setTimeout(() => {
                 this.imagem_heroi = './imagens/pose_sailormoon.jpg';
+            }, 3000);
+        },
+        ativar_gif_vilao(imagem){
+            this.imagem_vilao = imagem;
+            setTimeout(() => {
+                this.imagem_vilao = './imagens/koan_pose.png';
             }, 4000);
         },
         acao_vilao() {
             const acoes = ['atacar', 'atacar','defender', 'defender', 'usar_pocao', 'correr'];
             const acaoAleatoria = acoes[Math.floor(Math.random() * acoes.length)];
+            if (acaoAleatoria === 'atacar' && this.heroi.defendendo) {
+                this.acao_vilao();
+                return;
+            }
             this[acaoAleatoria](false);
         },
         fim_de_jogo(){
@@ -78,9 +120,12 @@ createApp({
                 }
                 this.heroi.vida = 100;
                 this.vilao.vida = 100;
-                this.jogo_ativo = true;
+                this.jogo_ativo = false;
                 this.quantidade_pocao = 2;
-                this.botao_desativado = false;
+                this.quantidade_pocao_vilao = 2;
+                this.botao_desativado = true;
+                this.imagem_heroi = './imagens/pose_sailormoon.jpg';
+                this.imagem_vilao = './imagens/koan_pose.png'; 
             }
         },
         iniciar_timer(){
@@ -100,7 +145,12 @@ createApp({
                                 this.turno = true;
                                 this.botao_desativado = false;
                             }
+                        } else {
+                            this.botao_clicado = false; 
                         }
+                    }
+                    if (!this.turno) {
+                        this.acao_vilao();
                     }
                 }
             }, 1000);
