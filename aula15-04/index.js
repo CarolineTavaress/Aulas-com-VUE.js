d14c27b5508dba44576fc617c6232be7a7f430b4
Caroline Tavaress <ca.tavares7960@gmail.com>
2024-04-27 13:15:56 -0300

    aulas vue

--- aula15-04/index.js
+++ aula15-04/index.js
@@ -24,7 +24,9 @@ createApp({
                     const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${(this.nextPage - 1) * 60}&limit=60`)
                     const data = await response.json();
                     const pokemon_details_promises = data.results.map(async pokemon => this.fetch_pokemon_data(pokemon.url))
-                    console.log(pokemon_details_promises)
+                    const pokemon_details = await Promise.all(pokemon_details_promises)
+                    this.pokemons = [... this.pokemons, ... pokemon_details];
+                    this.nextPage++
                 } catch (error){
                     console.error(error)
                 }
@@ -38,12 +40,35 @@ createApp({
                         name: data.name,
                         weight: data.weight,
                         types: data.types,
-                        sprite: data.sprite,
-                        show_details: false,
+                        sprite: data.sprites,
+                        show_details: true,
                     }
                 } catch (error){
                     console.error(error)
                 }
+            },
+            getTypeClass(type){
+                const typeClassMap = {
+                    fire: 'fire',
+                    grass: 'grass',
+                    water: 'water',
+                    bug: 'bug',
+                    normal: 'normal',
+                    poison: 'poison',
+                    electric: 'electric',
+                    fairy: 'fairy',
+                    fighting: 'fighting',
+                    ground: 'ground',
+                    rock: 'rock',
+                    ghost: 'ghost',
+                    dragon: 'dragon',
+                    ice: 'ice',
+                    psychic: 'psychic',
+                    steel: 'steel',
+                    dark: 'dark',
+                    flying: 'flying'
+                }
+                return typeClassMap[type] || ''
             }
     }
 }).mount("#app")
