e650e466349a56b2562631bd56566679e46a297b
Caroline Tavaress <ca.tavares7960@gmail.com>
2024-05-19 12:18:06 -0300

    atualizando o git

---
+++ aula18-05/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Olá, Mundo!"/>
+  <HelloWorld msg="Estou aprendendo vue!"/>
+  <HelloWorld msg="Jubileu"/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ aula18-05/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  type: String,
+  required: true
+})
+</script>
+
+<template>
+  <p class="texto">{{msg}}</p>
+</template>
+
+<style scoped>
+.texto{
+  color:#FF4500;
+  font-style:italic;
+}
+</style>
