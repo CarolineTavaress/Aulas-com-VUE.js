8774463436eedebb5ab5fd058d24e87f96bcaac5
Caroline Tavaress <ca.tavares7960@gmail.com>
2024-06-02 16:54:19 -0300

    atualizando git

---
+++ aula25-05/src/components/mensagem.vue
@@ -0,0 +1,21 @@
+<script>
+import Menssagem from "../components/menssagem.vue";
+import Usuario from "../components/usuario.vue";
+export default {
+    name: "App",
+    components:{
+        Menssagem,
+    },
+    data(){
+        return{
+            idade: 10
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Valores entre componentes</h1>
+    <Messagem :idade="idade"></Messagem>
+    <Usuario :idade="idade" @trocar_idade="idade++"></Usuario>
+</template>
---
+++ aula25-05/src/components/usuario.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    name: 'Usuario',
+    props: ["idade"],
+    emits: ["trocar_idade"],
+    methods:{
+        onclickIdade(){
+            this.$emit("trocar_idade");
+        }
+    }
+}
+</script>
+
+<template>
+    <button type="button" @click="onClickIdade">Update Idade</button>
+    <p>O usuário tem {{ idade }} anos</p>
+</template>
